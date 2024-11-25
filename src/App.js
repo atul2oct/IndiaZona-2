24b83e1d5b8b4979b26f9a0a8a4b15ea7bfccc8b
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2024-11-26 00:43:17 +0530

    both modal completed
    both modal completed + search bar completed

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import "./App.css";
 import ReturnForm from "./component/ReturnForm";
 import Navbar from "./component/Navbar";
 import Error from "./Page/Error";
+import Dashboard from "./Page/Dashboard";
 
 
 function App() {
+  
   return (
     <main className='w-screen min-h-screen bg-white text-[#A1A1A1] flex flex-col
     font-inter'>
@@ -15,7 +17,8 @@ function App() {
     
       <Route path='/' element={<Navbar/>}>
         {/* Route for return */}
-        
+
+        <Route path='/dashboard' element={<Dashboard/>}/>        
         <Route path='/dashboard/my-profile' element={<ReturnForm/>}/>
         <Route path='*' element={<Error/>}/>
       </Route>
